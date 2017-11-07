62351126a9c961eaff080cae7a643953b141f78e
Yuxuan Zhang <zyuxuan@flux-login1.arc-ts.umich.edu>
2017-11-07 17:21:13 -0500

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,57 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 #include <iostream>
-#include <omp.h>
-using namespace std;
-int A[100000], B[100000];
-int main() {
+#include <fstream>
+#include <string> 
 
-	omp_set_num_threads(10);
-	#pragma omp parallel for schedule(dynamic, 10)
-	for (int i = 0; i < 100; ++i) {
-		int num = omp_get_thread_num();
-		B[i] = i;
-		A[i] = B[i]+1;
-		#pragma omp critical
-		cout << i<<endl; 
-	}
-		
+using namespace std;
+
+void* DoWork(void* args){
+	printf("x\n");
+	string filename = "a.txt";
+	
+	ofstream myfile(filename.c_str());
+	myfile<<"a\n";
+	myfile.close();
 
 	return 0;
 }
 
+
+void* DoWorkB(void* args){
+	printf("x\n");
+	string filename = "b.txt";
+	
+	ofstream myfile(filename.c_str());
+	myfile<<"b\n";
+	myfile.close();
+	return 0;
+}
+
+
+
+int main(){
+	
+	pthread_t threadA;
+	pthread_t threadB;
+
+	pthread_attr_t attr;
+	cpu_set_t cpus;
+
+	pthread_attr_init(&attr);
+	CPU_ZERO(&cpus);
+	CPU_SET(0, &cpus);
+	pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+	pthread_create(&threadA, &attr, DoWork, NULL);
+
+	CPU_ZERO(&cpus);
+	CPU_SET(6, &cpus);
+	pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+	pthread_create(&threadB, &attr, DoWorkB, NULL);
+
+	pthread_join(threadA, NULL);
+	pthread_join(threadB, NULL);
+
+	return 0;
+}

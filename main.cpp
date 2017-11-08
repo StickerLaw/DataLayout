075ca26a1941b93a1bd270e3ed74a871022797e3
Yuxuan Zhang <zyuxuan@flux-login4.arc-ts.umich.edu>
2017-11-08 15:52:16 -0500

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,12 @@
 
 using namespace std;
 
+struct coreNumAndAddr{
+	int cpuNum;
+	int* addr;	
+};
+
 void* DoWork(void* args){
-	printf("x\n");
 	//string filename = "a.txt";
 	
 	//ofstream myfile(filename.c_str());
@@ -21,9 +25,18 @@ void* DoWork(void* args){
 
 
 void* assignData(void* args){
+	struct coreNumAndAddr* arg = (struct coreNumAndAddr*) args;
+	int cpuIdx = arg->cpuNum;
+	int* addr = arg->addr;
+	cout<<"new thread: "<<cpuIdx<<endl;
+	
+	addr = (int*)mmap(addr, 1000*sizeof(int), PROT_READ||PROT_WRITE, MAP_32BIT, 0, 0);
 
 	return 0;
 }
+
+
+
 
 
 
@@ -40,21 +53,29 @@ int main(){
 	cpu_set_t cpus;
 
 	// allocate two memory location
-	int* closeToNode0;
-	int* closeToNode1;
+	int* closeToNode0 = new int[1000];
+	int* closeToNode1 = new int[1000];
+
+	struct coreNumAndAddr parameter0;
+	struct coreNumAndAddr parameter1;
+
+	parameter0.cpuNum = 0;
+	parameter1.cpuNum = 6;
+	parameter0.addr = closeToNode0;
+	parameter1.addr = closeToNode1;
 
 	// create two threads each allocate memory address close to that core
 	pthread_attr_init(&attr);
 	CPU_ZERO(&cpus);
 	CPU_SET(0, &cpus);
 	pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
-	pthread_create(&threadOnCore0, &attr, assignData, NULL);
+	pthread_create(&threadOnCore0, &attr, assignData, (void*)&parameter0);
 
 	pthread_attr_init(&attr);
 	CPU_ZERO(&cpus);
 	CPU_SET(6, &cpus);
 	pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
-	pthread_create(&threadOnCore1, &attr, assignData, NULL);
+	pthread_create(&threadOnCore1, &attr, assignData, (void*)&parameter1);
 
 
 	// create a bunch of threads randomly assign to different cores.

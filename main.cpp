827c3083653ffac0901cf4ef4fff5ccb8d8628fb
Yuxuan Zhang <zyuxuan@flux-login3.arc-ts.umich.edu>
2017-11-08 17:45:10 -0500

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,38 @@
-#include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <iostream>
 #include <fstream>
 #include <string> 
 #include <sys/mman.h>
-
+#include <sys/types.h>
+#include <err.h>
 using namespace std;
 
 struct coreNumAndAddr{
-	int cpuNum;
-	int* addr;	
+	int* addr;
+	int** addrOfAddr;	
 };
 
 void* DoWork(void* args){
-	//string filename = "a.txt";
 	
-	//ofstream myfile(filename.c_str());
-	//myfile<<"a\n";
-	//myfile.close();
-
 	return 0;
 }
 
 
 void* assignData(void* args){
 	struct coreNumAndAddr* arg = (struct coreNumAndAddr*) args;
-	int cpuIdx = arg->cpuNum;
+	int** addrAddr = arg->addrOfAddr;
 	int* addr = arg->addr;
-	cout<<"new thread: "<<cpuIdx<<endl;
-	
-	addr = (int*)mmap(addr, 1000*sizeof(int), PROT_READ||PROT_WRITE, MAP_32BIT, 0, 0);
-
+	cout<<"new thread: "<<endl;
+	addr = (int*)mmap(addr, 1024*sizeof(int), PROT_READ|PROT_WRITE, MAP_ANON|MAP_SHARED, -1, 0);
+	if (addr == MAP_FAILED){
+		cout<<"map failed."<<endl;
+		err(1, "mmap");
+	}
+	for (int i=0; i<1000; i++){
+		addr[i] = i;
+	}
+	*addrAddr = addr;
 	return 0;
 }
 
@@ -53,14 +54,19 @@ int main(){
 	cpu_set_t cpus;
 
 	// allocate two memory location
-	int* closeToNode0 = new int[1000];
-	int* closeToNode1 = new int[1000];
-
+	int* closeToNode0;// = new int[1000];
+	int* closeToNode1;// = new int[1000];
+/*
+	for (int i=0; i<1000; i++){
+		closeToNode0[i] = i;
+		closeToNode1[i] = i+3000;
+	}
+*/
 	struct coreNumAndAddr parameter0;
 	struct coreNumAndAddr parameter1;
 
-	parameter0.cpuNum = 0;
-	parameter1.cpuNum = 6;
+	parameter0.addrOfAddr = &closeToNode0;
+	parameter1.addrOfAddr = &closeToNode1;
 	parameter0.addr = closeToNode0;
 	parameter1.addr = closeToNode1;
 
@@ -76,6 +82,21 @@ int main(){
 	CPU_SET(6, &cpus);
 	pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
 	pthread_create(&threadOnCore1, &attr, assignData, (void*)&parameter1);
+
+	pthread_join(threadOnCore0, NULL);
+	pthread_join(threadOnCore1, NULL);
+
+	for (int i =0; i<1000; i++){
+		int** tmp = parameter0.addrOfAddr;
+		if (tmp ==NULL) cout<<"hhhhhh"<<endl;
+		cout<<(*tmp)[i]<<endl;
+	}
+
+
+//	for (int i =0; i<1000; i++){
+//		cout<<closeToNode1[i]<<endl;
+//	}
+
 
 
 	// create a bunch of threads randomly assign to different cores.
